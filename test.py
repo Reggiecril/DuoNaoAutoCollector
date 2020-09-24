89630b58e50a8a0e840264c119d7dc09168073c9
yuncheng.xie <yuncheng.xie@datayes.com>
2020-09-24 13:37:46 +0800

    movie list

--- test.py
+++ test.py
@@ -1,23 +1,29 @@
-from chrome_driver import ChromeDriver
+from browsermobproxy import Server
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver, server, proxy = ChromeDriver().get_driver()
-base_url = 'https://www.ifvod.tv/list?keyword=&star=&page={0}&pageSize=30&cid=0,1,3&year=-1&language=-1&region=-1&status=-1&orderBy=2&desc=true'.format(
-    1)
-driver.get(base_url)
+server = Server(r'/root/browsermob-proxy-2.1.4/bin/browsermob-proxy')
+server.start()
+proxy = server.create_proxy()
+
+chrome_options = Options()
+chrome_options.add_argument('--ignore-ssl-errors=yes')
+chrome_options.add_argument('--ignore-certificate-errors')
+prefs = {'profile.managed_default_content_settings.images': 2}
+chrome_options.add_experimental_option('prefs', prefs)
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])  # 以开发者模式
+chrome_options.add_argument('--proxy-server={0}'.format(proxy.proxy))
+driver = webdriver.Chrome(options=chrome_options)
+
+base_url = 'https://www.ifvod.tv/list?keyword=&star=&page=1&pageSize=30&cid=0,1,3&year=-1&language=-1&region=-1&status=-1&orderBy=2&desc=true'
 proxy.new_har("datayes", options={'captureHeaders': True, 'captureContent': True})
-result = proxy.har
-while True:
-    if result['log']['entries'] is None or len(result['log']['entries']) <= 0:
-        result = proxy.har
-    flag = False
-    for entry in result['log']['entries']:
-        _url = entry['request']['url']
-        if "api/list/Search" in _url:
-            flag = True
-    if flag:
-        break
-    result = proxy.har
+driver.get(base_url)
 
+result = proxy.har
 
 for entry in result['log']['entries']:
     _url = entry['request']['url']
@@ -27,5 +33,5 @@ for entry in result['log']['entries']:
         _content = _response['content']['text']
         # 获取接口返回内容
         print(_content)
-driver.close()
+
 server.stop()
