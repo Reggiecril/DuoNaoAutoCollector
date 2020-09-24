b9ee5e6f576b192b68dc1afa63d8451ad7c62b5c
yuncheng.xie <yuncheng.xie@datayes.com>
2020-09-24 11:15:32 +0800

    movie list

--- chrome_driver.py
+++ chrome_driver.py
@@ -1,25 +1,27 @@
+from browsermobproxy import Server
 from selenium import webdriver
 
 from selenium.webdriver.chrome.options import Options
 
 
 class ChromeDriver:
-    def __init__(self):
-        """
-        构建chrome driver
-        :param window_w: 设置窗口宽度
-        :param window_h: 设置窗口高度
-        :param wait_sec: 最大等待时间，单位：秒
-        """
-        self.chrome_options = Options()
-        prefs = {'profile.managed_default_content_settings.images': 2}
-        self.chrome_options.add_experimental_option('prefs', prefs)
-        self.chrome_options.add_argument('--no-sandbox')
-        self.chrome_options.add_argument('--headless')
-        self.chrome_options.add_argument('--disable-gpu')
-        self.chrome_options.add_argument('--disable-dev-shm-usage')
-        self.chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])  # 以开发者模式
-
     def get_driver(self):
-        return webdriver.Chrome(options=self.chrome_options)
+        server = Server(r'/root/browsermob-proxy-2.1.4/bin/browsermob-proxy')
+        server.start()
+        proxy = server.create_proxy()
+
+        chrome_options = Options()
+        chrome_options.add_argument('--ignore-ssl-errors=yes')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        prefs = {'profile.managed_default_content_settings.images': 2}
+        chrome_options.add_experimental_option('prefs', prefs)
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])  # 以开发者模式
+        chrome_options.add_argument('--proxy-server={0}'.format(proxy.proxy))
+        drive = webdriver.Chrome(options=chrome_options)
+        proxy.new_har("datayes", options={'captureHeaders': True, 'captureContent': True})
+        return drive, proxy
         #
--- movie_list.py
+++ movie_list.py
@@ -1,66 +1,52 @@
 # coding:utf-8
-import json
-import sys
-import time
 
-from bs4 import BeautifulSoup
 from selenium.common.exceptions import TimeoutException
+
+from chrome_driver import ChromeDriver
 
 
 class MovieList:
-    def __init__(self, driver):
+    def __init__(self):
         # 初始化Chrome
-        self.driver = driver
+        self.driver, self.proxy = ChromeDriver().get_driver()
         self.driver.set_page_load_timeout(60)
-        url = 'https://www.ifvod.tv/list?keyword=&star=&page=1&pageSize=30&cid=0,1,3&year=-1&language=-1&region=-1&status=-1&orderBy=2&desc=true'
-        try:
-            self.driver.get(url)
-            f = open('url.txt', 'w+')
-            f.close()
-        except TimeoutException:
-            print(u'页面加载超过设定时间，超时')
-            # 当页面加载时间超过设定时间，
-            # 通过执行Javascript来stop加载，然后继续执行后续动作
+        f = open('url.txt', 'w+')
+        f.close()
 
     def get_movie_list(self, count=1, page=1):
+        url = 'https://www.ifvod.tv/list?keyword=&star=&page={0}&pageSize=30&cid=0,1,3&year=-1&language=-1&region=-1&status=-1&orderBy=2&desc=true'.format(
+            page)
+        try:
+            self.driver.get(url)
+        except TimeoutException:
+            print("超时")
         movie_url_list = list()
-        timer = 0
-        while len(movie_url_list) <= 0 or movie_url_list is None:
-            source = BeautifulSoup(self.driver.page_source, "lxml")
-            time.sleep(0.5 + timer / 10)
-            movie_url_list = source.select(
-                'app-root > app-search > div > div.page-container.list > div.inner.d-flex.flex-wrap > div > div.search-results.d-flex.flex-wrap.justify-content-between.ng-star-inserted > app-video-teaser > div > a ')
-            timer += 1
-            if timer % 20 == 0:
-                self.driver.execute_script("location.reload()")
-            if timer >= 100:
-                return
-        with open('url.txt', 'a+') as file:
-            url_list = list()
-            for i in movie_url_list:
-                map = dict()
-                map['id'] = i.get('href').split("=")[1]
-                map['hot_count'] = count
-                url_list.append(map)
-                count += 1
-            file.write(json.dumps(url_list, ensure_ascii=False) + '\n')
-            print(json.dumps(url_list))
-        next_page = self.driver.find_elements_by_css_selector(
-            'body > div.root-container > app-root > app-search > div > div.page-container.list > div.inner.d-flex.flex-wrap > div > div.d-flex.mb-5.page-controls.align-items-center.justify-content-center.ng-star-inserted > app-pager > ul > li')[
-            -2]
-        if next_page.get_attribute('class') == 'disabled':
-            self.driver.close()
-            sys.exit(0)
-        else:
-            try:
-                page += 1
-                url = 'https://www.ifvod.tv/list?keyword=&star=&page=%s&pageSize=30&cid=0,1,3&year=-1&language=-1&region=-1&status=-1&orderBy=2&desc=true', page
-                self.driver.get(url)
-            except TimeoutException:
-                print(u'页面加载超过设定时间，超时')
+        result = self.proxy.har
+        for entry in result['log']['entries']:
+            _url = entry['request']['url']
+            # 根据URL找到数据接口
+            if "api/list/Search" in _url:
+                _response = entry['response']
+                _content = _response['content']['text']
+                # 获取接口返回内容
+                print(_content)
+        # with open('url.txt', 'a+') as file:
+        #     url_list = list()
+        #     for i in movie_url_list:
+        #         map = dict()
+        #         map['id'] = i.get('href').split("=")[1]
+        #         map['hot_count'] = count
+        #         url_list.append(map)
+        #         count += 1
+        #     file.write(json.dumps(url_list, ensure_ascii=False) + '\n')
+        #     print(json.dumps(url_list))
+        page += 1
+        self.get_movie_list(count, page)
 
-            try:
-                self.get_movie_list(count, page)
-            except Exception:
-                self.driver.execute_script("location.reload()")
-                self.get_movie_list(count, page)
+    def __del__(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    movie = MovieList()
+    movie.get_movie_list()
