ff2a08140131d23028606080ef54123468900f74
yuncheng.xie <yuncheng.xie@datayes.com>
2020-09-24 18:57:48 +0800

    change timeout

--- movie_list.py
+++ movie_list.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import json
+import math
 import time
 
 import requests
@@ -14,7 +15,7 @@ class MovieList:
         f = open('url.txt', 'w+')
         f.close()
 
-    def get_movie_list(self, page=1):
+    def get_movie_list(self, page=1, limit=1):
         url = 'https://www.ifvod.tv/list?keyword=&star=&page={0}&pageSize=30&cid=0,1,3&year=-1&language=-1&region=-1&status=-1&orderBy=2&desc=true'.format(
             page)
         self.driver.get(url)
@@ -48,16 +49,31 @@ class MovieList:
             self.driver, self.server, self.proxy = ChromeDriver().get_driver()
             self.get_movie_list(page)
             return
-        next_page = self.driver.find_elements_by_css_selector(
-            'body > div.root-container > app-root > app-search > div > div.page-container.list > div.inner.d-flex.flex-wrap > div > div.d-flex.mb-5.page-controls.align-items-center.justify-content-center.ng-star-inserted > app-pager > ul > li')[
-            -2]
-        if next_page.get_attribute('class') == 'disabled':
+        if page > limit:
             self.driver.quit()
             self.server.stop()
             self.proxy.close()
         page += 1
         self.get_movie_list(page)
 
+    def get_limitation(self):
+        url = 'https://www.ifvod.tv/list?keyword=&star=&page={0}&pageSize=30&cid=0,1,3&year=-1&language=-1&region=-1&status=-1&orderBy=2&desc=true'.format(
+            1)
+        self.driver.get(url)
+        self.proxy.new_har("datayes-1", options={'captureHeaders': True, 'captureContent': True})
+        result = self.proxy.har
+        time_start = time.time()
+        while time.time() - time_start < 60:
+            if 'log' in result is None or 'entries' in result['log']:
+                result = self.proxy.har
+            for entry in result['log']['entries']:
+                if 'request' in entry and 'url' in entry['request']:
+                    _url = entry['request']['url']
+                    if "api/list/Search" in _url:
+                        r = requests.get(_url)
+                        return math.ceil(int(r.json()['data']['info'][0]['recordcount']) / 30)
+
+
 if __name__ == '__main__':
     movie = MovieList()
-    movie.get_movie_list()
+    movie.get_movie_list(movie.get_limitation())
