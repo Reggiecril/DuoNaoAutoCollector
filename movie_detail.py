95e78035a363207fdfea2d7863380fe1ac4e74c3
reggiecril <reggiecril0618@gmail.com>
2020-10-11 20:15:16 +0800

    add pk with database

--- movie_detail.py
+++ movie_detail.py
@@ -128,20 +128,22 @@ class MovieDetail:
     def send_file(self, file_name):
         client = paramiko.SSHClient()  # 获取SSHClient实例
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect("122.51.155.8", username="ubuntu", password="Cloud19961008")  # 连接SSH服务端
+        client.connect(hostname="122.51.155.8", username="ubuntu", password="Cloud19961008", port=22)  # 连接SSH服务端
         transport = client.get_transport()  # 获取Transport实例
 
         # 创建sftp对象，SFTPClient是定义怎么传输文件、怎么交互文件
         sftp = paramiko.SFTPClient.from_transport(transport)
-
+        file_name = file_name[:-1]
         for root, dirs, files in os.walk(file_name):
-            path = '~/' + root.replace(file_name, '') + '/'
-            if path == '~//':
-                path = '~/'
+            path = '/home/ubuntu' + root.replace(file_name, '') + '/'
+            if path == '/home/ubuntu//':
+                path = '/home/ubuntu/'
             print('当前目录路径', path)  # 当前目录路径
             print('当前路径下所有非目录子文件', files)  # 当前路径下所有非目录子文件
             for i in files:
-                sftp.put(path + i, file_name + i)
+                print(path + i)
+                print(root + '/' + i)
+                sftp.put(root + '/' + i, path + i)
         sftp.close()
         # 关闭连接
         client.close()
--- test.py
+++ test.py
@@ -1,10 +1,32 @@
 import os
 
+import paramiko
+
+
+def send_file(file_name):
+    client = paramiko.SSHClient()  # 获取SSHClient实例
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname="122.51.155.8", username="ubuntu", password="Cloud19961008", port=22)  # 连接SSH服务端
+    transport = client.get_transport()  # 获取Transport实例
+
+    # 创建sftp对象，SFTPClient是定义怎么传输文件、怎么交互文件
+    sftp = paramiko.SFTPClient.from_transport(transport)
+
+    for root, dirs, files in os.walk(file_name):
+        path = '/home/ubuntu' + root.replace(file_name, '') + '/'
+        if path == '/home/ubuntu//':
+            path = '/home/ubuntu/'
+        print('当前目录路径', path)  # 当前目录路径
+        print('当前路径下所有非目录子文件', files)  # 当前路径下所有非目录子文件
+        for i in files:
+            print(path + i)
+            print(root + '/' + i)
+            sftp.put(root + '/' + i, path + i)
+    sftp.close()
+    # 关闭连接
+    client.close()
+
+
 count = 0
 url = '/Users/xieyuncheng/Desktop/github/python/duoNaoAutoCollector/project/'
-path = '~/project'
-for root, dirs, files in os.walk(url):
-    count += 1
-    print(count)
-    print('当前目录路径', path + root.replace(url, '') + '/')  # 当前目录路径
-    print('当前路径下所有非目录子文件', files)  # 当前路径下所有非目录子文件
+print(url[:-1])
